19b4dcb83fb17ce62737bdeb7ed182aaa0f0b78a
gokhan94 <gokhan.ileri94@gmail.com>
2023-06-01 00:54:20 +0300

    application uploaded to server

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -56,7 +56,7 @@ const userLogin = async (req, res) => {
    
         const token = createJWT(user._id)
         res.cookie("accessToken", token, {
-            domain: 'https://react-pos-management-system.vercel.app/',
+            domain: '.react-pos-management-system-qmcf.com',
             httpOnly: true,
             sameSite: 'none',
             maxAge: 30 * 24 * 60 * 60 * 1000,
