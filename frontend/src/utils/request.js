beb857526cf401a6dc65123a17791952009b970e
gokhan94 <gokhan.ileri94@gmail.com>
2023-06-01 12:51:32 +0300

    application uploaded to server

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -9,7 +9,9 @@ import axios from 'axios'
 
 const httpRequest = axios.create({
     headers: {
-         "Content-Type": "application/json"
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
     },
     baseURL: "https://react-pos-management-system.vercel.app/api",
     withCredentials: true,
