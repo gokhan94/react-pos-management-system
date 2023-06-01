18c69e91bd1750bf8a68b906e9ea9d2cdf3d6475
gokhan94 <gokhan.ileri94@gmail.com>
2023-06-01 12:55:04 +0300

    application uploaded to server

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -11,6 +11,7 @@ const httpRequest = axios.create({
     headers: {
         "Accept": "application/json",
         "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "https://react-pos-management-system-qmcf.vercel.app",
         "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
     },
     baseURL: "https://react-pos-management-system.vercel.app/api",
