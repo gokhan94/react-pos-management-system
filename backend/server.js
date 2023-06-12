c0239546f97c593786efad3fe9e50251371731e5
gokhan94 <gokhan.ileri94@gmail.com>
2023-06-12 19:28:33 +0300

    application uploaded to server

--- backend/server.js
+++ backend/server.js
@@ -23,13 +23,6 @@ const corsOrigin = {
     credentials:true,            
 }
 app.use(cors(corsOrigin))
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next()
-})
-
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
