1a2cff54bcd85d12ba4fbb1c3cd3d3d149d707ae
gokhan94 <gokhan.ileri94@gmail.com>
2023-06-15 22:10:26 +0300

    application uploaded to server

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,20 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+  if (req.method == "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+
+  next();
+});
+
 // ROUTES
 app.get('/', (req, res) => {
     res.send('Welcome')
